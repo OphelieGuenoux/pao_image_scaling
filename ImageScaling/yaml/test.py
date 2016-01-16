d0718ae779d1bf25db38670bc9c045d34fbaca34
opetit <olivier.petit@insa-rouen.fr>
2016-01-16 11:57:57 +0100

    Début d'organisation

--- ImageScaling/yaml/test.py
+++ ImageScaling/yaml/test.py
@@ -1,30 +1,46 @@
 from SuperScale import SuperScale
+import os
+from scipy.misc import imread
 
 
-SuperScale.upscaling("./dataset/images_input/5.png", "./tests")
-SuperScale.upscaling("./dataset/images_input/15.png", "./tests")
-SuperScale.upscaling("./dataset/images_input/25.png", "./tests")
-SuperScale.upscaling("./dataset/images_input/35.png", "./tests")
-SuperScale.upscaling("./dataset/images_input/1456.png", "./tests")
-SuperScale.upscaling("./dataset/images_input/1654.png", "./tests")
-SuperScale.upscaling("./dataset/images_input/1780.png", "./tests")
-SuperScale.upscaling("./dataset/images_input/1802.png", "./tests")
-SuperScale.upscaling("./dataset/images_input/1994.png", "./tests")
+#SuperScale.upscaling("./dataset/images_input/5.png", "./tests")
+#SuperScale.upscaling("./dataset/images_input/15.png", "./tests")
+#SuperScale.upscaling("./dataset/images_input/25.png", "./tests")
+#SuperScale.upscaling("./dataset/images_input/35.png", "./tests")
+#SuperScale.upscaling("./dataset/images_input/1456.png", "./tests")
+#SuperScale.upscaling("./dataset/images_input/1654.png", "./tests")
+#SuperScale.upscaling("./dataset/images_input/1780.png", "./tests")
+#SuperScale.upscaling("./dataset/images_input/1802.png", "./tests")
+#SuperScale.upscaling("./dataset/images_input/1994.png", "./tests")
 
-SuperScale.upscaling("./tests/images/57-0.png", "./tests")
-SuperScale.upscaling("./tests/images/57-1.png", "./tests")
-SuperScale.upscaling("./tests/images/57-2.png", "./tests")
-SuperScale.upscaling("./tests/images/57-3.png", "./tests")
-SuperScale.upscaling("./tests/images/57-4.png", "./tests")
-SuperScale.upscaling("./tests/images/5_grosse.png", "./tests", 64, 64, 80, 80)
-SuperScale.upscalingRGB("./tests/images/oiseau.png", "./tests", 256, 256, 320, 320)
-SuperScale.upscaling("./tests/images/53.png", "./tests")
-SuperScale.upscaling("./tests/images/177.png", "./tests")
-SuperScale.upscaling("./tests/images/119.png", "./tests")
-SuperScale.upscaling("./tests/images/212.png", "./tests")
-SuperScale.upscaling("./tests/images/1241.jpg", "./tests")
-SuperScale.upscalingRGB("./tests/images/rectangleR.png","./tests")
+#SuperScale.upscaling("./tests/images/57-0.png", "./tests")
+#SuperScale.upscaling("./tests/images/57-1.png", "./tests")
+#SuperScale.upscaling("./tests/images/57-2.png", "./tests")
+#SuperScale.upscaling("./tests/images/57-3.png", "./tests")
+#SuperScale.upscaling("./tests/images/57-4.png", "./tests")
+#SuperScale.upscaling("./tests/images/5_grosse.png", "./tests", 64, 64, 80, 80)
+#SuperScale.upscalingRGB("./tests/images/oiseau.png", "./tests", 256, 256, 320, 320)
+#SuperScale.upscaling("./tests/images/53.png", "./tests")
+#SuperScale.upscaling("./tests/images/177.png", "./tests")
+#SuperScale.upscaling("./tests/images/119.png", "./tests")
+#SuperScale.upscaling("./tests/images/212.png", "./tests")
+#SuperScale.upscaling("./tests/images/1241.jpg", "./tests")
+#SuperScale.upscalingRGB("./tests/images/rectangleR.png","./tests")
 
+#for im in os.listdir("./dataset/images_test_input"):
+#	SuperScale.upscaling("./dataset/images_test_input/"+im, "./vrai_test")
+
+i = 0
+erreur = []
+for im in os.listdir("./dataset/images_test_output"):
+	indice = im.split(".")[0]
+	imagePred = imread("./vrai_test/"+indice+"_reconstruction.png", flatten=1)
+	imageVrai = imread("./dataset/images_test_output/"+im, flatten=1)
+	erreur.append(sum(sum(pow(imageVrai-imagePred, 2)))/(40*40))
+	i = i + 1
+mse = sum(erreur)/len(erreur)
+print("Erreur quadratique moyenne de test : ")
+print(mse)
 
 #roundval = 100
 
